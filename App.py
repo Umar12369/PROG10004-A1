ea3ce7587c1f92a854f5ce7e0753aae878272463
Umar12369 <Omar_aizan@hotmail.com>
2022-10-14 19:53:16 -0400

    one role created for warrior

--- App.py
+++ App.py
@@ -1,5 +1,11 @@
 
+from Game import Warrior_Dice
+from Game import SRK_Dice
+from Game import Pakola_Dice
+from Game import Jojo_Dice
 
+from random import random
+from tkinter.messagebox import YES
 
 print("Welcome to KingZone Dragon X")
 adventure = input("Would you like to go on a adventure? Yes/No ")  
@@ -10,3 +16,62 @@ if adventure == "yes":
         print("You are SubZero a bounty hunter Who's mission is to kill lord JOJO to claim your gold!")
         print (" You excel in Defense Attack and Stamina")
     jojo = input("Are you ready to face jojo? Yes/No  ")
+    if jojo == YES:
+        print("You will first need to beat one of jojo's minions called SRK")
+    SRK = input("Are you ready to fight SRK?   Yes/NO  ")
+    if SRK == YES:
+        print(" You will need to roll a dice first ")
+        print( " Depending on the number of the dice is how strong your attack will be")
+        print("rolling Dice  ")
+
+    if Warrior_Dice > SRK_Dice:
+        print("Warrior wins")
+    elif SRK_Dice > Warrior_Dice:
+        print("SRK wins")
+    else:
+        print("It's a draw")
+
+    print("Game Over")
+
+
+    next = input("Would you like to continue?  Yes/No    ")
+    if next == YES:
+        print ("Good Job making to the next round!")
+    
+        print( "Now you're against JOJO'S right hand man called Pakola ")
+
+    a = input("you are now going against Pakola! Are you Ready?  YES/NO  "  )
+    if a == YES:
+        print(" goodluck!")
+
+    print("warrior rolled: ", Warrior_Dice)
+    print("Pakola rolled: ", Pakola_Dice)
+
+    if Warrior_Dice > SRK_Dice:
+        print("Warrior wins")
+    elif SRK_Dice > Warrior_Dice:
+        print("PAKOLA wins")
+    else:
+        print("It's a draw")
+
+print("Game Over")
+j = input("Would you like to continue? Yes/NO  ")
+if j == YES:
+    print("You are now against the Final BOSS!! JOJO!!!")
+
+c = input(" ARE you ready to go against the Final BOSS JOJO? YES/NO "  )
+if c == YES:
+    print("GOODLUCK!!")
+
+print("warrior rolled: ", Warrior_Dice)
+print("JOJO rolled: ", Jojo_Dice)
+
+if Warrior_Dice > Jojo_Dice:
+    print("Warrior wins")
+elif Jojo_Dice > Warrior_Dice:
+    print("PAKOLA wins")
+else:
+    print("It's a draw")
+
+print("Game Over")
+print("Thank You For Playing")
---
+++ Game.py
@@ -0,0 +1,52 @@
+import random
+
+Warrior_Dice = random.randint(2,12)
+SRK_Dice = random.randint(2,12)
+
+print("warrior rolled: ", Warrior_Dice)
+print("SRK rolled: ", SRK_Dice)
+
+if Warrior_Dice > SRK_Dice:
+    print("Warrior wins")
+elif SRK_Dice > Warrior_Dice:
+    print("SRK wins")
+else:
+    print("It's a draw")
+
+print("Game Over")
+
+
+
+Warrior_Dice = random.randint(2,12)
+Pakola_Dice = random.randint(2,12)
+
+print("warrior rolled: ", Warrior_Dice)
+print("Pakola rolled: ", Pakola_Dice)
+
+if Warrior_Dice > SRK_Dice:
+    print("Warrior wins")
+elif SRK_Dice > Warrior_Dice:
+    print("PAKOLA wins")
+else:
+    print("It's a draw")
+
+print("Game Over")
+
+
+
+
+
+Warrior_Dice = random.randint(2,12)
+Jojo_Dice = random.randint(2,12)
+
+print("warrior rolled: ", Warrior_Dice)
+print("Jojo rolled: ", Jojo_Dice)
+
+if Warrior_Dice > Jojo_Dice:
+    print("Warrior wins")
+elif Jojo_Dice > Warrior_Dice:
+    print("JOJO wins")
+else:
+    print("It's a draw")
+
+print("Game Over")
