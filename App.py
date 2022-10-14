5eb5423a38b6f755e26a0de79149986ad7cc7a55
Umar12369 <Omar_aizan@hotmail.com>
2022-10-14 18:58:10 -0400

    user input

---
+++ App.py
@@ -0,0 +1,11 @@
+
+
+
+print("Welcome to KingZone Dragon X")
+adventure = input("Would you like to go on a adventure? Yes/No ")  
+if adventure == "yes":
+    print("Pick your adventure")
+    option = input("fighting or Saving?  ")
+    if option == "fighting":
+        print("You are SubZero a bounty hunter Who's mission is to kill lord JOJO to claim your gold!")
+    jojo = input("Are you ready to face jojo? Yes/No  ")
